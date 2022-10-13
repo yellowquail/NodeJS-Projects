3ca407ae37aef3c75e696b532e42a3554150857b
Dairenya Shou <84019537+dairenyashou@users.noreply.github.com>
2022-10-14 03:54:52 +0700

    Update dairenyashou.js

--- dairenyashou.js
+++ dairenyashou.js
@@ -1,35 +1,20 @@
-"use strict";
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-function toLower(inputText) {
-    let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    let temp = "";
-    for (let i = 0; i < inputText.length; i++) {
-        let isLower = true;
-        for (let j = 0; j < upper.length; j++) {
-            if (inputText.charAt(i) === upper[j]) {
-                isLower = false;
-                temp += lower[j];
-            }
-        }
-        if (isLower) {
-            temp += inputText.charAt(i);
-        }
-    }
-    return temp;
+function cashbackCounter(cb, price) {
+    return 'Rp. ' + Number((cb / 100) * price).toLocaleString('id');
 }
 
-function isLetterArray(inputText) {
-    let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    let text = toLower(inputText);
-    for (let i = 0; i < lower.length; i++) {
-        if (text[0] === lower[i]) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    return temp;
-}
-console.log(isLetterArray(9))
+rl.question('Price : ', (price) => {
+    rl.question('Casback : ', (cashback) => {
+        const result = cashbackCounter(cashback, price)
+        console.log('================[ Cashbac Counter ]================');
+        console.log('Price : ', 'Rp.', Number(price).toLocaleString('id'));
+        console.log('Cashback : ', cashback + '%');
+        console.log('Result : ', result)
+        rl.close();
+    });
+});

a2417425371969a534dfc37698c8859e0c9d4a72
Dairenya Shou <84019537+dairenyashou@users.noreply.github.com>
2022-10-14 03:54:18 +0700

    Update dairenyashou.js

--- dairenyashou.js
+++ dairenyashou.js
@@ -1,35 +1,93 @@
-"use strict";
+const fetch = require("node-fetch");
+const from = require("form-data");
+const cheerio = require("cheerio");
+const tanya = require("readline-sync");
+let data = new from();
 
-function toLower(inputText) {
-    let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    let temp = "";
-    for (let i = 0; i < inputText.length; i++) {
-        let isLower = true;
-        for (let j = 0; j < upper.length; j++) {
-            if (inputText.charAt(i) === upper[j]) {
-                isLower = false;
-                temp += lower[j];
+let user = tanya.question("Username? ");
+let pass = tanya.question("Password? ");
+data.append("ajaran", "2020");
+data.append("username", user); //0037205240
+data.append("password", pass); //123456
+
+(async () => {
+  console.log(`Mencoba Login. . .`);
+  await fetch("http://icpasuruan.hopto.org:8094/login/do_login/", {
+    method: "POST",
+    body: data,
+  }).then(async (res) => {
+    console.log(`Sukses Login!`);
+    let cookie = await res.headers.get("set-cookie");
+    await fetch("http://icpasuruan.hopto.org:8094/student/cheknotif", {
+      method: "GET",
+      headers: {
+        cookie: cookie,
+      },
+    })
+      .then((res) => res.text())
+      .then(async (res) => {
+        console.log(`Cek Notif Terbaru. . .`);
+        let jumlah_notif = res;
+        jumlah_notif = parseInt(jumlah_notif);
+        if (jumlah_notif == 0) {
+          console.log(`Tidak Ada Notif Terbaru`);
+        } else {
+          console.log(`Anda Memiliki ${jumlah_notif} Notifikasi Baru`);
+          await fetch("http://icpasuruan.hopto.org:8094/student/semuanotif", {
+            method: "GET",
+            headers: {
+              cookie: cookie,
+            },
+          })
+            .then((res) => res.text())
+            .then(async (res) => {
+              const $ = cheerio.load(res);
+              let allNotif = $("div.notification-info").text();
+              allNotif = allNotif.replace(/\s\s+/g, "\n");
+              allNotif = allNotif.split("Yang Lalu\n");
+              for (let i = 0; i < jumlah_notif; i++) {
+                console.log(`${allNotif[i]}Yang Lalu`);
+              }
+            });
+        }
+        console.log(`Membuka page tugas`);
+        await fetch("http://icpasuruan.hopto.org:8094/studentmaster/grid_ki3", {
+          method: "GET",
+          headers: {
+            cookie: cookie,
+          },
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            let jumlah = res.recordsTotal;
+            for (let i = 0; i < jumlah; i++) {
+              console.log(`Guru : ${res.data[i][1]}
+Kelas : ${res.data[i][2].replace(/<br \/>\n/g, "")}
+Keterangan : ${res.data[i][4]}
+Waktu : ${res.data[i][5]}
+Status : ${res.data[i][6].split(">")[1].split("<")[0]} 
+              `);
             }
-        }
-        if (isLower) {
-            temp += inputText.charAt(i);
-        }
-    }
-    return temp;
-}
-
-function isLetterArray(inputText) {
-    let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    let text = toLower(inputText);
-    for (let i = 0; i < lower.length; i++) {
-        if (text[0] === lower[i]) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    return temp;
-}
-console.log(isLetterArray(9))
+          });
+        console.log(`Membuka page tugas keterampilan`);
+        await fetch("http://icpasuruan.hopto.org:8094/studentmaster/grid_ki4", {
+          method: "GET",
+          headers: {
+            cookie: cookie,
+          },
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            let jumlah = res.recordsTotal;
+            for (let i = 0; i < jumlah; i++) {
+              console.log(`Guru : ${res.data[i][1]}
+Kelas : ${res.data[i][2].replace(/<br \/>\n/g, "")}
+Keterangan : ${res.data[i][4]}
+Waktu : ${res.data[i][5]}
+Status : ${res.data[i][6].split(">")[1].split("<")[0]}                
+                `);
+            }
+          });
+      });
+  });
+})(); 
